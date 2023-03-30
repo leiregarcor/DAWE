98dc03e05e0546a49c8edb7983dfc9a07effa7ac
leire <leiregarciaupv@gmail.com>
2023-03-30 22:55:03 +0200

    comentarios varios

--- 9_lab/Semana_9-node_express_mongo/public/js/main.js
+++ 9_lab/Semana_9-node_express_mongo/public/js/main.js
@@ -4,15 +4,19 @@ function $class(clase) {
 }
 
 window.onload = function() {
+	//obtenemos los links de delete de la lista de usuarios
 	let links = document.getElementsByClassName("deleteUser");
+	//por cada link añadimos un eventlistener:
 	for (let item of links) {
-	    item.addEventListener("click", deleteUser);
+		//si se hace click llama a la función deleteUser
+	    item.addEventListener("click", deleteUser); 
 	}
 
-	//obtenemos los links de la lista de usuarios
+	//obtenemos los links de edit de la lista de usuarios
 	let editLinks = $class("editUser");
 	//por cada link añadimos un eventlistener:
 	for (let item of editLinks) {
+		//si se hace click llama a la función editUser
 	    item.addEventListener("click", editUser);
 	}
 }
@@ -37,4 +41,7 @@ function deleteUser(event){
 
 function editUser(event) {
 	console.log('editar')	;
+	var url = '/users/edit/' + event.target.getAttribute('data-id');
+	//console.log(url)	;
+
 }
