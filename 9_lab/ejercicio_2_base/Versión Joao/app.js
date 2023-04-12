f0e3e202ff837ada98b7623e3b39a0c00ec54d49
VitorAzevedo76 <joao.76.azevedo@gmail.com>
2023-04-12 17:57:04 +0200

    Labo 9 Ejer 2 sin acabar
    Falta hacer upload de las fotos

---
+++ 9_lab/ejercicio_2_base/Versión Joao/app.js
@@ -0,0 +1,192 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var multer = require('multer');
+//var upload = multer();
+const storageEngine = multer.diskStorage({
+  destination: "./public/imgs/",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  },
+});
+//const upload = multer({ dest: '/public/imgs/' })
+const upload = multer({
+		storage: storageEngine,
+});
+// Concatenar paths
+var path = require("path"); 
+// Modulo para realizas validaciones en formularios
+const { check, validationResult } = require('express-validator');
+// Modulo para conectar con una base de datos MongoDB
+const mongojs = require('mongojs')
+// Objeto exportado de mongojs para identificar un elemento de una tabla de una BD
+var ObjectId = mongojs.ObjectId;
+
+// Conexion con la base de datos: nombre de la BD y de la tabla
+const db = mongojs('clientesapp', ['users'])
+
+app = express();
+
+// Middleware que carga ficheros estaticos de un directorio (public en este caso).
+// Es decir, podemos cargar los elementos que haya en public/
+app.use(express.static(path.join(__dirname, "public")));
+
+// View Engine 
+app.set('view engine', 'ejs'); // motor de plantillas
+app.set('views', path.join(__dirname, "views")); // carpeta donde guardar las vistas
+
+// Middleware para el parseo de req.body
+app.use(express.json()); // coge los datos en crudo y los pasa a json
+
+// Que parsee datos que lleguen en la query HTTP y los deje como un objeto JSON 
+app.use(express.urlencoded({extended: false})); 
+
+app.use(bodyParser.urlencoded());
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: false })); 
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array()); 
+app.use(express.static('public'));
+
+// Declaracion y definicion de variables globales: en este caso errors
+app.use(function (req, res, next) {
+	res.locals.errors = null;
+	next();
+ });
+
+var users = [
+    {
+   	 id: 1,
+   	 first_name: 'John',
+   	 last_name: 'Doe',
+   	 email: 'johndoe@gmail.com',
+    },
+    {
+   	 id: 2,
+   	 first_name: 'Bob',
+   	 last_name: 'Smith',
+   	 email: 'bobsmith@gmail.com',
+    },
+    {
+   	 id: 3,
+   	 first_name: 'Jill',
+   	 last_name: 'Jackson',
+   	 email: 'jjackson@gmail.com',
+    }
+];
+
+
+app.post('/users/add', [
+	check("first_name", "El nombre es obligatorio").notEmpty(),
+	check("last_name", "El apellido es obligatorio").notEmpty(),
+	check("email", "El email es obligatorio").notEmpty()
+],
+function(req, res) {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		res.render('index', {
+			title:'clientes',
+			users: users,
+			errors: errors.array()
+		});
+	} else {
+		var newUser = {
+			"first_name" : req.body.first_name,
+			"last_name" : req.body.last_name,
+			"email" : req.body.email,
+		};
+		db.users.insertOne(newUser, function(err, resp) {
+			if(err) {
+				console.log(err);
+			} else {
+				db.users.insertOne(newUser);
+			}
+			res.redirect('/');
+		});
+		console.log(newUser)
+	}
+});
+
+app.delete('/users/delete/:id', function(req, res) {
+	db.users.remove({_id: ObjectId(req.params.id)}, function(err, result) {
+		if(err) {
+			console.log(err);
+		}
+		res.redirect(303, '/');
+	});
+});
+
+
+app.patch('/users/update/:id', function(req, res) {
+	db.users.remove({_id: ObjectId(req.params.id)}, function(err, result) {
+		if(err) {
+			console.log(err);
+		}
+		res.redirect(303, '/');
+	});
+});
+
+app.put("/upload/files",function(req, res){
+	console.log("Servidor Recibido")
+	console.log(req.body)
+	//res.send(users)
+	var newUser = {
+			"first_name" : req.body.first_name,
+			"telefono" : req.body.telefono,
+			"email" : req.body.email,
+			"books": req.body.books,
+			"cantidad": req.body.cantidad
+
+		};
+		db.users.insertOne(newUser, function(err, resp) {
+			if(err) {
+				console.log(err);
+			} else {
+				db.users.insertOne(newUser);
+			}
+			//res.redirect('/');
+		});
+	newUser["exito"] = true;
+	res.json(newUser)
+
+});
+app.get("/", function(req, res) { // peticion y respuesta como parametros
+    db.users.find(function(err, docs) {
+    	if(err) {
+    		console.log(err);
+    	} else {
+    		console.log(docs);
+    		// para rellenar la plantilla
+    		res.render('index', {
+			//title: 'clientes',
+			//users: docs
+    		});
+    	}
+    });
+    
+});
+
+app.get("/users/update/:id", function(req, res) { // peticion y respuesta como parametros
+    db.users.find(function(err, docs) {
+    	if(err) {
+    		console.log(err);
+    	} else {
+    		console.log("docs");
+    		// para rellenar la plantilla
+    		//res.render('index', {
+			//title: 'clientes',
+			//users: docs
+    		//});
+    	}
+    });
+    
+});
+    
+app.listen(3000, function(){ // a la escucha en el puerto 3000
+	console.log("Servidor lanzado en el puerto 3000");
+});
