c6cc493345de4d184325db4421a15ea75b8ddba4
VitorAzevedo76 <joao.76.azevedo@gmail.com>
2023-04-18 19:19:28 +0200

    Labo 10 Acabado (validarlo por si acaso)

--- 10_lab/postits.js
+++ 10_lab/postits.js
@@ -3,6 +3,7 @@ var y;
 window.onload = init;
 
 function init() {
+	localStorageSpace()
 	var button = document.getElementById("add_button");
 	button.onclick = createSticky;
 	var clear = document.getElementById("clear_button");
@@ -40,13 +41,36 @@ function addStickyToDOM(value) {
 	span.innerHTML = value;
 	postit.appendChild(span);
 	stickies.appendChild(postit);
+	localStorageSpace();
+
 }
 
 function clearStickyNotes() {
 	// Crear un nuevo botón en la ventana de postit notes que al pulsarlo,
 	// elimine las notas de pantalla y de localStorage
-	// Algoritmo:	
+	for (let i = 1; i <= y; i++) {
+		localStorage.removeItem("postit_" + i);
+		document.getElementById("stickies").removeChild(document.getElementById("stickies").childNodes[1]) 
+	}
+	localStorage.setItem("length", 0)
+	y=0;
+	document.getElementById("size").innerHTML= 'Espacio Utilizado: Empty (0 KB)'
+
 	// obtener una referencia a la capa "stickies"
 	// recorrer los hijos (childNodes) de esa referencia,
 	// eliminándolos uno a uno (removeChild)
 }
+
+var localStorageSpace = function(){
+        var allStrings = '';
+        for(var key in window.localStorage){
+            if(window.localStorage.hasOwnProperty(key)){
+                allStrings += window.localStorage[key];
+            }
+        }
+       
+        var resp = allStrings ?  ((allStrings.length*16)/(8*1024)) + ' KB' : 'Empty (0 KB)'
+        document.getElementById("size").innerHTML= 'Espacio Utilizado: '  + resp 
+        
+ };
+
