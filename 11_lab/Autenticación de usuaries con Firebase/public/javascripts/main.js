43f05b3cc932b182db31b8e6a9308655211a763b
leire <leiregarciaupv@gmail.com>
2023-04-27 16:23:33 +0200

    terminado

---
+++ 11_lab/Autenticación de usuaries con Firebase/public/javascripts/main.js
@@ -0,0 +1,78 @@
+// getElementByClassName
+function $class(clase) {
+	return document.getElementsByClassName(clase);
+}
+// getElementById
+function $id(id) {
+	return document.getElementById(id);
+}
+
+window.onload = function() {
+	//obtenemos los links de delete de la lista de usuarios
+	let links = document.getElementsByClassName("deleteUser");
+	//por cada link añadimos un eventlistener:
+	for (let item of links) {
+		//si se hace click llama a la función deleteUser
+	    item.addEventListener("click", deleteUser); 
+	}
+
+	//obtenemos los links de edit de la lista de usuarios
+	let editLinks = $class("editUser");
+	//por cada link añadimos un eventlistener:
+	for (let item of editLinks) {
+		//si se hace click llama a la función editUser
+	    item.addEventListener("click", editUser);
+	}
+
+}
+
+
+
+function deleteUser(event){
+    var confirmation = confirm('Are You Sure?');
+	if(confirmation){
+		var url = '/users/delete/' + event.target.getAttribute('data-id');
+		var consulta = new XMLHttpRequest();
+		consulta.open("DELETE", url);
+		consulta.onload = function() {
+			if (consulta.status == 200) {
+				window.location.replace('/')
+			}
+		};
+		consulta.send();
+	} else {
+		return false;
+	}
+
+}
+
+function editUser(event) {
+	//document.getElementById("editar").style.display = "none";
+	//boton enviar no visible
+	let botonEnviar = $id("submit");
+	botonEnviar.value = "Edit";
+
+	
+	// campos del formulario
+	var usuario = $id("first_name");
+	var apellido = $id("last_name");
+	var email = $id("email");
+
+	//console.log('editar')	;
+	var url = '/users/getUser/' + event.target.getAttribute('data-id');
+	fetch(url)
+		.then((response)=> response.json())
+		.then((data)=> {
+			
+			usuario.value = data.first_name;
+			apellido.value = data.last_name;
+			email.value = data.email;
+
+			let form = $id("form");
+			form.action = "/user/edit/" + event.target.getAttribute('data-id');
+			
+		});
+
+	
+
+}
--- 11_lab/Autenticación de usuaries con Firebase/routes/index.js
+++ 11_lab/Autenticación de usuaries con Firebase/routes/index.js
@@ -1,7 +1,27 @@
 var express = require('express');
+
+
+// Modulo para realizas validaciones en formularios
+const { check, validationResult } = require('express-validator');
+
+// Modulo para conectar con una base de datos MongoDB
+const mongojs = require('mongojs')
+// Objeto exportado de mongojs para identificar un elemento de una tabla de una BD
+var ObjectId = mongojs.ObjectId;
+
+// Conexion con la base de datos: nombre de la BD y de la tabla
+const db = mongojs('clientesrouter', ['users']); // base de datos y collection
+
+
 var router = express.Router();
 var session = require('express-session');
 const MongoStore = require('connect-mongo');
+
+// Declaracion y definicion de variables globales: en este caso errors
+router.use(function (req, res, next) {
+	res.locals.errors = null;
+	next();
+ });
 
 var admin = require("firebase-admin");
 
@@ -14,7 +34,8 @@ router.use(session({
   secret: 'clavesecretaparaexpresss',
   saveUninitialized: true, // create session even if there is nothing stored
   resave: true, // save session even if unmodified
-  cookie: { maxAge: 60 * 60 * 1000 }
+  cookie: { maxAge: 60 * 60 * 1000 },
+  store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/test-router'})
 }));
 
 router.get('/',(req,res) => {
@@ -29,6 +50,108 @@ router.post('/sesion',(req,res) => {
   res.end('done');
 });
 
+router.post('/users/add', [
+	//validamos que los campos no están vacíos con el módulo que hemos instalado y cargado
+	check("first_name", "El nombre es obligatorio").notEmpty(),
+	check("last_name", "El apellido es obligatorio").notEmpty(),
+	check("email", "El email es obligatorio").notEmpty()
+	],
+	function(req, res) {
+		const errors = validationResult(req); //obtenemos posibles errores
+		if (!errors.isEmpty()) { //en caso de que el array de errores contenga algún error
+			res.render('index',  //index nombre del archivo, manda la vista renderizada al cliente
+			//pasamos variables locales a la vista en formato JSON
+			{	title:'clientes',
+				users: users, //esta mal
+				errors: errors.array()
+			});
+		} else {
+			var newUser = {
+				"first_name" : req.body.first_name,
+				"last_name" : req.body.last_name,
+				"email" : req.body.email,
+			};
+			db.users.insertOne(newUser, function(err, resp) {
+				if(err) {
+					console.log(err);
+				} else {
+					db.users.insertOne(newUser);
+				}
+				res.redirect('/');
+			});
+			console.log(newUser)
+		}
+	}
+);
+
+router.delete('/users/delete/:id', function(req, res) {
+	db.users.remove({_id: ObjectId(req.params.id)}, function(err, result) {
+		if(err) {
+			console.log(err);
+		}
+		res.redirect(303, '/');
+	});
+});
+
+router.post('/user/edit/:id', function(req, res) {
+	
+	db.users.update(
+		{_id: ObjectId(req.params.id)},
+	 	{$set: {first_name: req.body.first_name,
+				last_name: req.body.last_name,
+				email : req.body.email
+			   }
+		},  
+		function(err, result) {
+			if(err) {
+				console.log(err);
+			}
+			res.redirect(303, '/');
+	});
+	
+});
+
+router.get('/users/getUser/:id', function(req, res) {
+	//db.users.findOne({_id:ObjectId("642736c4e08120bee822d931")})
+	db.users.findOne({_id: ObjectId(req.params.id)}, function(err, result) {
+		if(err) {
+    		console.log(err);
+    	} else {
+    		//console.log(result); // lo que mongodb nos devuelva
+    		
+    		// mandar los datos en texto plano
+    		res.send(result);
+			//manda la repsuesta en json
+			//res.json(usuario);
+    	}
+	});
+	
+});
+
+//enrutamiento: función callback a una petición de la página raiz
+router.get("/admin", function(req, res) { // peticion y respuesta como parametros
+  if(req.session.email) {
+    db.users.find(function(err, docs) { // si la consulta falla-> error tendrá un objeto de error; en caso contrario, docs contendrá el documento encontrado
+      if(err) {
+        console.log(err);
+      } else {
+        //console.log(docs); // lo que mongodb nos devuelva
+        // para rellenar la plantilla
+        res.render('index', { //JSON con tuplas que se pasan como variables
+        title2: 'Insertar cliente',
+        title: 'clientes',
+        users: docs,
+        helloemail: req.session.email
+        });
+      }
+    });
+  }else {
+    res.write('<h1>Please login first.</h1>');
+    res.end('<a href='+'/email-password.html'+'>Login</a>');
+  }
+  
+});
+/*
 router.get('/admin',(req,res) => {
   if(req.session.email) {
     res.write(`<h1>Hello ${req.session.email} </h1><br>`);
@@ -38,14 +161,14 @@ router.get('/admin',(req,res) => {
     res.write('<h1>Please login first.</h1>');
     res.end('<a href='+'/'+'>Login</a>');
   }
-});
+});*/
 
-router.get('/?logout',(req,res) => {
+router.get('/logout',(req,res) => {
   req.session.destroy((err) => {
     if(err) {
       return console.log(err);
     }
-    res.redirect('/');
+    res.redirect('/email-password.html?logout');
   });
 
 });
@@ -53,7 +176,7 @@ router.get('/?logout',(req,res) => {
 router.post('/getToken', (req, res) => {
   const idToken = req.body.idToken; // capturar parámetro
 
-// idToken comes from the client app
+// idToken comes from the client router
 // verificamos el idToken para ver si es válido
   admin.auth().verifyIdToken(idToken)
       .then(function (decodedToken) {
