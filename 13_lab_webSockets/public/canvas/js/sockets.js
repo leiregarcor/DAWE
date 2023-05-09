038721fa1273e1e8b097aa1314d2fb0de1f2342f
leire <leiregarciaupv@gmail.com>
2023-05-09 23:38:13 +0200

    txiki intento

--- 13_lab_webSockets/public/canvas/js/sockets.js
+++ 13_lab_webSockets/public/canvas/js/sockets.js
@@ -5,5 +5,22 @@ export function setupSockets(){
     const socket = io.connect(serverURL, {secure: true});
     // register phone connection
     socket.emit('desktop-connect');
+
+    // recibimos el movimiento del movil //TODO no funciona no se como hacerlo
+    socket.on("phone-move", function () {
+        // Primero simulamos que pisamos la tecla
+        window.dispatchEvent(
+            new KeyboardEvent('keydown',
+                {key: 'ArrowRight'
+            })
+        );
+        // Y luego que dejamos de pisarla (levantamos el dedo)
+        window.dispatchEvent(
+            new KeyboardEvent('keyup',
+                {key: 'ArrowRight'
+            })
+        );
+        
+    });
     
 }
